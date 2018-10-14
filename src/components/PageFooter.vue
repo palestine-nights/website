9cc7f3850e1c9561c7add9f96aa50b9c8f535960
Ali Shanaakh <github@shanaakh.pro>
2018-10-14 15:00:58 +0300

    Integrate google maps rating component  (#2)
    * Add hardcoded rating to the page
    
    * Fix footer
    
    * Finilize golang rating service integration

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -4,7 +4,7 @@
     <v-btn :href="icon.link" v-for="icon in icons" :key="icon.link" class="mx-3 white--text" icon>
       <v-icon size="24px">{{ icon.icon }}</v-icon>
     </v-btn>
-    <v-flex lighten-2 py-3 text-xs-center white--text xs12>
+    <v-flex lighten-2 py-3 text-xs-center grey--text xs12>
       &copy;{{ year }} — All rights reserved
     </v-flex>
     </v-layout>
