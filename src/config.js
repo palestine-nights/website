4a3f78250c960be88877f08d7ddf00598ca47c2d
Ali Shanaakh <ashanaakh@gmail.com>
2018-10-26 20:36:28 +0300

    Redirect to GoogleMaps on rating click

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ function getPlacesHost() {
   if (process.env.NODE_ENV === 'production') {
     return 'https://places.palestinenights.com';
   }
-  return 'http://localhost:8000';
+  return 'http://localhost:3000';
 }
 
 module.exports = {
